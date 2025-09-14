7d57917de17c18ea629f64a7631c87f327f43b45
Alger Makiputin <hello@algermakiputin.dev>
2025-09-14 20:17:14 +0800

    update prisma shcema

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -2,6 +2,7 @@ import { IsNumber } from 'class-validator';
 import { CreateOrderItemDto } from 'src/order-item/dto/create-order-item.dto';
 export class CreateOrderDto {
   @IsNumber()
-  total: number;
+  totalAmount: number;
+  subTotal: number;
   orderItem: CreateOrderItemDto[];
 }
